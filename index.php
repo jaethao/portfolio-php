79258598a3b2a939fb70fbe76bdd2a7e8dae2f2e
jaethao <mjathao@gmail.com>
2018-07-30 16:06:09 -0500

    nav bar and header

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- Paper Kit -->
+    <link rel="stylesheet" href="assets/css/paper-kit.css">
+    <!-- Animation CSS -->
+    <link rel="stylesheet" href="assets/css/animations.css">
+    <!-- Style sheet -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Jae Thao's Portfolio</title>
+  </head>
+  <body>
+    <!-- nav bar -->
+    <nav class="navbar navbar-expand-md navbar-default">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar-menu" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Jae Thao</a>
+    <div class="collapse navbar-collapse" id="navbar-menu">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Portfolio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Bruno's Game</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<!-- wrapper -->
+<div class="wrapper">
+  <div class="page-header" style="background-image: url('assets/images/oceanBg.jpg')">
+        <img src="assets/images/tubegirl.png" class="img-fluid tossing" style="width: 500px">
+        <img src="assets/images/bruno-swim.png" class="img-fluid floating" style="width: 300px">
+    </div>
+  </div>
+
+
+
+    <!-- JQuery -->
+    <script type="text/javascript" src="assets/javascript/jquery-3.2.1.js"></script>
+    <!-- Paper Kit -->
+    <!-- <script type="text/javascript" src="assets/javascript/paper-kit.js"></script> -->
+    <!-- Bootstrap JS -->
+    <script type="text/javascript" scr="assets/javascript/bootstrap.min.js"></script>
+    <!-- Popper -->
+    <script type="text/javascript" src="assets/javascript/popper.js"></script>
+    <!-- JS Logic -->
+    <script type="text/javascript" src="assets/javascript/logic.js"></script>
+  </body>
+</html>
